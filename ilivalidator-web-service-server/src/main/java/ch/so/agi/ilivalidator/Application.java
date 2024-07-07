98b63902c11abbbb4627f5e4083a3c7ff63e2ab7
Stefan Ziegler <edi.gonzales@gmail.com>
2024-07-07 19:01:36 +0200

    add timeout properties

--- ilivalidator-web-service-server/src/main/java/ch/so/agi/ilivalidator/Application.java
+++ ilivalidator-web-service-server/src/main/java/ch/so/agi/ilivalidator/Application.java
@@ -1,8 +1,11 @@
 package ch.so.agi.ilivalidator;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,7 +15,21 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class Application {
   
+    @Value("${app.connectTimeout}")
+    private String connectTimeout;
+    
+    @Value("${app.readTimeout}")
+    private String readTimeout;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }  
+    
+    @Bean
+    CommandLineRunner init() {
+        return args -> {
+            System.setProperty("sun.net.client.defaultConnectTimeout", connectTimeout);
+            System.setProperty("sun.net.client.defaultReadTimeout", readTimeout);
+        };
+    }
 }
